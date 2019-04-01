402bfe695417830bbcf32d5297f840f90562dd98
Chris Gruel <christopher_a_gruel@homedepot.com>
2019-04-01 16:46:00 -0400

    add network link output

--- outputs.tf
+++ outputs.tf
@@ -30,6 +30,10 @@ output "network_name" {
   value = "${google_compute_network.vpc.name}"
 }
 
+output "network_link" {
+  value = "${google_compute_network.vpc.self_link}"
+}
+
 output "subnet_name" {
   value = "${google_compute_subnetwork.subnet.name}"
 }
