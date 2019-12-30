083dd1c05f6fd32f784d3b1f6d58d40277035350
Chris Gruel <chris_gruel@homedepot.com>
2019-12-30 17:43:25 -0500

    add region

--- tf-nat.tf
+++ tf-nat.tf
@@ -7,8 +7,9 @@ resource "google_compute_address" "nat" {
 }
 
 data "google_compute_address" "existing_nat" {
-  count = var.cloud_nat_address_name != "" ? 1 : 0
-  name  = var.cloud_nat_address_name
+  count . = var.cloud_nat_address_name != "" ? 1 : 0
+  name    = var.cloud_nat_address_name
+  region  = var.region
 }
 
 # Create a NAT router so the nodes can reach DockerHub, etc
